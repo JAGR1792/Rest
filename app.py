0bd99aca2d664870bc746791341a1b360914ff5e
JAGR1792 <jorgenopehd@gmail.com>
2025-07-31 18:11:47 -0500

    bye bye

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import requests
 app = Flask(__name__)
 
 def fetch_data(url):
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.json()
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
 
 @app.route('/')
 def index():
@@ -16,24 +16,20 @@ def index():
 
 @app.route('/Paises')
 def home():
-    
-
     countries = fetch_data("https://restcountries.com/v3.1/all?fields=name,capital,flags")
     countries.sort(key=lambda x: x['name']['common'])
     
     return render_template("home.html", countries=countries)
 
-@app.route('/Paises/<country_name>')
-def Paises(country_name):
-    url = f"https://restcountries.com/v3.1/name/{country_name}?fullText=true"
+@app.route('/Paises/<pais>')
+def paises_detalle(pais):
+    
+    url = f"https://restcountries.com/v3.1/name/{pais}?fullText=true"
     datos = fetch_data(url)
-
     if datos:
         return render_template("detalles.html", country=datos[0])
     else:
-        return f"No se encontró información para {country_name}"
-
-    
+        return f"No se encontró información para {pais}"
 
 
 if __name__ == '__main__':
